a04253f3559321ca4cd616f8c2ac40f2ae75cd55
Julian Cook <juliantcook@gmail.com>
2026-05-06 15:22:03 +0100

    don't override zshrc because other installers add junk to it

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ln -s .zshrc ~/.zshrc
+ln -s .myrc ~/.myrc
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .gitconfig ~/.gitconfig
 
+zshrc="${ZDOTDIR:-$HOME}/.zshrc"
+source_line="source ~/.myrc"
+
+if [ -e "$zshrc" ] && grep -Fxq "$source_line" "$zshrc"; then
+  echo "$source_line already present in $zshrc"
+else
+  echo "$source_line" >> "$zshrc"
+fi
